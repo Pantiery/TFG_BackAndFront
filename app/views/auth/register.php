cc84015ad8ff8d3543c19de093b7eecd31ec1722
javichut00 <jcuadrosgarcia@educa.madrid.org>
2026-03-26 11:35:10 +0100

    lease enter the commit message for your ch:wq# with '#' will be ignored, and an empty message aborts the commit.

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -1,24 +1,24 @@
 <?php require_once __DIR__ . '/../layout/header.php'; ?>
 
-    <form method="POST" action="/proyecto_TFG/TFG_BackAndFront/public/register" class="grid-layout">
+    <form method="POST" action="/proyecto_TFG/TFG_BackAndFront/public/register" class="grid-layout" onsubmit="return validarForm()" novalidate>
             
         <h2>Regístrate</h2>
-
+        <div id="div"></div>
         <label>Nombre</label>
-        <input type="text" name="nombre" class="form-control" maxlength="25" required>
-
+        <input type="text" name="nombre" id="nombre" class="form-control" required>
+        <div id="div1"></div>
         <label>Primer apellido</label>
-        <input type="text" name="apellido1" class="form-control" maxlength="25" required>
-
+        <input type="text" name="apellido1" id="apellido1" class="form-control" required>
+        <div id="div2"></div>
         <label>Segundo apellido (opcional)</label>
-        <input type="text" name="apellido2" class="form-control" maxlength="25">
-
+        <input type="text" name="apellido2" id="apellido2" class="form-control">
+        <div id="div3"></div>
         <label>E-mail</label>
-        <input type="email" name="email" class="form-control" maxlength="100" required>
-
+        <input type="email" name="email" id="email" class="form-control" required>
+        <div id="div4"></div>
         <label>Contraseña</label>
-        <input type="password" name="password" class="form-control" minlength="8" pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$" required>
-
+        <input type="password" name="password" id="password" class="form-control" required>
+        <div id="div5"></div>
         <button type="submit" class="btn btn-primary">Registrarme</button>
 
     </form>
