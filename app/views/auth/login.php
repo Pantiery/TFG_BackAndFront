3a551e6cfdb5779ea00a0b1821855f3098c95916
javichut00 <jcuadrosgarcia@educa.madrid.org>
2026-03-27 14:19:50 +0100

    Ahora el mensaje del error de 'credenciales incorrectas' ahora sale dentro del formulario

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,23 +1,23 @@
 <?php require_once __DIR__ . '/../layout/header.php'; ?>
 
-<?php if (isset($_SESSION['error_credenciales'])): ?>
-  <p style="color:red;">
-    <?= $_SESSION['error_credenciales'] ?>
-  </p>
-  <?php unset($_SESSION['error_credenciales']); ?>
-<?php endif; ?>
 
 <form method="POST" class="grid-layout">
-
+  
   <h2>Login</h2>
-
+  
   <label>Email</label>
   <input type="email" name="email" class="form-control" required>
-
+  
   <label>Contraseña</label>
   <input type="password" name="password" class="form-control" required>
-
+  
   <button type="submit" class="btn btn-primary">Entrar</button>
+  <?php if (isset($_SESSION['error_credenciales'])): ?>
+    <p style="color:red;">
+      <?= $_SESSION['error_credenciales'] ?>
+    </p>
+    <?php unset($_SESSION['error_credenciales']); ?>
+  <?php endif; ?>
 
 </form>
 
